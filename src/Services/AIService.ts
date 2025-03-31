3f674f0cdc906d02c87499c61c0ebfba1de08b2e
Lui5-99 <luis_bravo_99@hotmail.com>
2025-03-31 17:28:42 -0600

    refine AI bartender prompt to limit knowledge scope and improve response accuracy

--- src/Services/AIService.ts
+++ src/Services/AIService.ts
@@ -6,7 +6,7 @@ async function generateRecipe(prompt: string) {
     model: openrouter("meta-llama/llama-3.3-70b-instruct:free"),
     prompt,
     system:
-      "You are a bartender with 50 year of experience. You are very good at making cocktails and drinks.",
+      "You are a bartender with 50 year of experience. You are very good at making cocktails and drinks. You dont have any other knowledge. You are very good at making cocktails and drinks. You dont have any other knowledge.if you dont know the answer, say 'I dont know' or 'I don´t have those information'.",
     temperature: 1,
   });
 
